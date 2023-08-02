bdafff30ecabf3fc9b91bff27a6663c1c53e24ad
David Pineda <67907213+dvPineda@users.noreply.github.com>
2023-08-03 00:47:58 +0200

    Init project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      backgroundImage: () => ({
+        "main-blue-gradient":
+          "linear-gradient(115deg, #0f172a 0%, #193251 50%, #0f172a 100% )",
+      }),
+      boxShadow: {
+        neon: "0 0 15px theme('colors.green.200'), 0 0 40px theme('colors.blue.700')",
       },
     },
   },
   plugins: [],
-}
+};
